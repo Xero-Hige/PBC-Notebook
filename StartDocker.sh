9d1e2690ed5c3d5f42fa498a09c6ff851dfce8f4
Xero-Hige <gaston.martinez.90@gmail.com>
2016-08-27 00:54:56 -0300

    Changed image name

--- StartDocker.sh
+++ StartDocker.sh
@@ -1,1 +1,1 @@
-docker run --net="host" -v $(pwd):/notebook -p 8888:8888 -p 6006:6006 xerohige/basic-notebook sh -c "jupyter-notebook /notebook"
+docker run --net="host" -v $(pwd):/notebook -p 8888:8888 -p 6006:6006 xerohige/pbc-notebook sh -c "jupyter-notebook /notebook"
